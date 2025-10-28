3b65062d4ddf9828ff93f932e69a6bcf068344ca
NilDACE <nil.vollhardt@gmail.com>
2025-10-28 13:51:49 +0100

    feat: dialogs added

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let pictureArr = [
   "./img/Pictures/clouds.jpg",
   "./img/Pictures/dust.jpg",
@@ -11,17 +10,46 @@ let pictureArr = [
   "./img/Pictures/mountain-lakes.jpg",
   "./img/Pictures/see.jpg",
   "./img/Pictures/sunset.jpg",
-  "./img/Pictures/villa.jpg"
+  "./img/Pictures/villa.jpg",
 ];
 
 function render() {
- let contentRef = document.getElementById("content")
- for (let index = 0; index < pictureArr.length; index++) {
+  let contentRef = document.getElementById("content");
+  for (let index = 0; index < pictureArr.length; index++) {
     contentRef.innerHTML += getNoteTemplate(index);
-    
- }    
+  }
 }
 
-function getNoteTemplate(index){
-    return `<img class="pictures" src="${pictureArr[index]}" alt="Bild ${index}">`;
+function getNoteTemplate(index) {
+  return `<img class="pictures" onclick="openDialog(${index})" src="${
+    pictureArr[index]
+  }" alt="Bild ${index}">
+    <dialog id="pictureDialog-${index}" class="picture-dialog" onclick="closeDialog(${index})">
+          <div class="dialog-container" onclick="event.stopPropagation()">
+          <header>
+            <h2>
+              ${pictureArr[index].slice(15)}
+            </h2>
+            <button class="button" onclick="closeDialog(${index})">X</button>
+          </header>
+          <section>
+            <img class="dialog-picture" src="${
+              pictureArr[index]
+            }" alt="Bild ${index}">
+          </section>
+          <footer>
+
+          </footer>
+          </div>
+        </dialog>`;
+}
+
+function openDialog(index) {
+  document.getElementById(`pictureDialog-${index}`).showModal();
+  document.getElementById(`pictureDialog-${index}`).classList.add("opened");
+}
+
+function closeDialog(index) {
+  document.getElementById(`pictureDialog-${index}`).close();
+  document.getElementById(`pictureDialog-${index}`).classList.remove("opened");
 }
